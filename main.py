0ab56802e590b153ae355e3cff70408071481604
saumyashovanroy <dpr0413@gmail.com>
2018-10-14 14:33:16 +0600

    rectangle

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ classNames = {0: 'background',
 
 
 def id_class_name(class_id, classes):
-    for key,value in classes.items():
+    for key, value in classes.items():
         if class_id == key:
             return value
 
@@ -31,17 +31,27 @@ model = cv2.dnn.readNetFromTensorflow('models/frozen_inference_graph.pb',
                                       'models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
 image = cv2.imread("image.jpeg")
 
+image_height, image_width, _ = image.shape
+
 model.setInput(cv2.dnn.blobFromImage(image, size=(300, 300), swapRB=True))
 output = model.forward()
 # print(output[0,0,:,:].shape)
 
+
 for detection in output[0, 0, :, :]:
     confidence = detection[2]
     if confidence > .5:
-        class_id = detection[1]
-        print(str(str(class_id) + " " + str(detection[2])  + " " + id_class_name(class_id,classNames)))
+        # class_id = detection[1]
+        # print(str(str(class_id) + " " + str(detection[2])  + " " + id_class_name(class_id,classNames)))
+        box_x = detection[3] * image_width
+        box_y = detection[4] * image_height
+        box_width = detection[5] * image_width
+        box_height = detection[6] * image_height
+        cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
+
+# cv2.rectangle(image, (int(10), int(80)), (int(60), int(180)), (23, 230, 210), thickness=1)
 
 
-# cv2.imshow('image',image)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('image', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
