5d40ffb46e91341b95ac754cd3e8eaf9ee3eca15
saumyashovanroy <dpr0413@gmail.com>
2018-10-14 15:08:05 +0600

    image class text

--- main.py
+++ main.py
@@ -41,17 +41,22 @@ output = model.forward()
 for detection in output[0, 0, :, :]:
     confidence = detection[2]
     if confidence > .5:
-        # class_id = detection[1]
-        # print(str(str(class_id) + " " + str(detection[2])  + " " + id_class_name(class_id,classNames)))
+        class_id = detection[1]
+        class_name=id_class_name(class_id,classNames)
+        print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
         box_x = detection[3] * image_width
         box_y = detection[4] * image_height
         box_width = detection[5] * image_width
         box_height = detection[6] * image_height
         cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width), int(box_height)), (23, 230, 210), thickness=1)
+        cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255))
 
-# cv2.rectangle(image, (int(10), int(80)), (int(60), int(180)), (23, 230, 210), thickness=1)
+
+
 
 
 cv2.imshow('image', image)
+# cv2.imwrite("image_box_text.jpg",image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
